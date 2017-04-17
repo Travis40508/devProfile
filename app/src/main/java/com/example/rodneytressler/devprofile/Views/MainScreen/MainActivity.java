8289eefb25b0563d009910f5f9cabbaff5ede2aa
Travis40508 <Rodney.Tressler@Interapthq.com>
2017-04-16 22:06:35 -0400

    Fragment Util finished.

--- app/src/main/java/com/example/rodneytressler/devprofile/Views/MainScreen/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/devprofile/Views/MainScreen/MainActivity.java
@@ -5,18 +5,21 @@ import android.os.Bundle;
 import android.widget.Toast;
 import com.example.rodneytressler.devprofile.Di.ApplicationClass;
 import com.example.rodneytressler.devprofile.R;
+import com.example.rodneytressler.devprofile.Tools.FragmentUtil;
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity implements MainView{
 
 
 @Inject protected MainPresenter mainPresenter;
+  private FragmentUtil fragmentUtil;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     ((ApplicationClass)getApplication()).getComponent().inject(this);
     setContentView(R.layout.activity_main);
 
+    fragmentUtil = new FragmentUtil(getSupportFragmentManager());
     mainPresenter.setView(this);
 
   }
